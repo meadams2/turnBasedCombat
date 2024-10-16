97748d862e85dcf3a906ceea958ad94c732c5fe6
meadams2 <meadams2@bsu.edu>
2024-10-16 17:26:49 -0400

    Defining Character class

--- tbc.py
+++ tbc.py
@@ -20,7 +20,40 @@ class Character(object):
         
     @property
     def maxDamage(self):
-        
+        return self._maxDamage
+    
+    @maxDamage.setter
+    def maxDamage(self, value):
+        if value == int:
+            if value >= 0:
+                self._maxDamage = value
+            else:
+                print ("Maximum Damage must be positive.")
+                self._maxDamage = 1
+        else:
+            print("Maximum damage must be a number.")
+            self._maxDamage = 1
+    
+    @property
+    def hitPoints(self):
+        return self._hitPoints
+    
+    @hitPoints.setter
+    def hitPoints(self, value):
+        if value == int:
+            self._hitPoints = value
+        else:
+            print("Hit Points must be a number.")
+            self._hitPoints = 10
+            
+    @property
+    def hitChance(self):
+        return self._hitChance
+    
+    @hitChance.setter
+    def hitChance(self, value):
+        if value == int:
+            
         
         
         
