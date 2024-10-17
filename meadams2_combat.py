a194d7d45325729445b186380ac68297216de41d
meadams2 <meadams2@bsu.edu>
2024-10-17 10:38:08 -0400

    Testing

--- meadams2_combat.py
+++ meadams2_combat.py
@@ -1,18 +1,18 @@
 """Marianne Adams
 CS120 Turn Based Combat System
 Combat Sequence"""
-import tbc
+import meadams2_tbc
 def main():
-    hero = tbc.Character()
-    hero.name = "Hero"
-    her.hitPoints = 10
-    hero.hitChance = 50
-    hero.maxDamage = 5
-    hero.armor = 2
-    monster = tbc.Character("Monster", 20, 30, 5, 0)
+#     hero = tbc.Character()
+#     hero.name = "Hero"
+#     her.hitPoints = 10
+#     hero.hitChance = 50
+#     hero.maxDamage = 5
+#     hero.armor = 2
+    monster = meadams2_tbc.Character("Monster", 20, 30, 5, 0)
     hero.printStats()
     monster.printStats()
-    tbc.fight(hero, monster)
+#     tbc.fight(hero, monster)
 
 if __name__ == "__main__":
     main()
