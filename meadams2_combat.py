fa4d9a586b5ef12f36f27194bbc5550c2223a0a4
meadams2 <meadams2@bsu.edu>
2024-10-17 16:19:36 -0400

    Testing main()
    Slight spelling error to fix. Fixed. Otherwise works as expected!!!

--- meadams2_combat.py
+++ meadams2_combat.py
@@ -3,16 +3,16 @@ CS120 Turn Based Combat System
 Combat Sequence"""
 import meadams2_tbc
 def main():
-#     hero = tbc.Character()
-#     hero.name = "Hero"
-#     her.hitPoints = 10
-#     hero.hitChance = 50
-#     hero.maxDamage = 5
-#     hero.armor = 2
+    hero = meadams2_tbc.Character()
+    hero.name = "Hero"
+    hero.hitPoints = 10
+    hero.hitChance = 50
+    hero.maxDamage = 5
+    hero.armor = 2
     monster = meadams2_tbc.Character("Monster", 20, 30, 5, 0)
     hero.printStats()
     monster.printStats()
-#     tbc.fight(hero, monster)
+    meadams2_tbc.fight(hero, monster)
 
 if __name__ == "__main__":
     main()
